0ef0c72d34e50bd12d4f0dd2e8d4a9309d30306e
Azamat Baiyzbekov <azamatbaiyzbekov@Azamats-MacBook-Pro.local>
2019-10-29 22:42:37 -0700

    connects to external API in search page

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,6 +1,34 @@
 import React, { Component } from 'react';
 
 class Search extends Component {
+    state = {
+        items: [],
+        isLoaded: false,
+        query: Number,
+        randomNum: Number,
+    };
+
+    componentDidMount() {
+        const num = Math.floor(Math.random() * 2221)
+        const url = `https://xkcd.now.sh/?comic=${num}`
+        fetch(url, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        })
+         .then(res => res.json())
+         .then(json => {
+             this.setState({
+                 isLoaded: true,
+                 items: json
+             })
+         });
+    };
+
+    
+
+
     render() {
         return(
             <div>Hi I am search page</div>
